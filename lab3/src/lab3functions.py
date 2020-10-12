52c02adc7c10f91d32abb75655de427b772d1459
Alexander López <lopez.alexander@pucp.edu.pe>
2020-10-12 13:23:02 -0500

    Update lab3functions.py

--- lab3/src/lab3functions.py
+++ lab3/src/lab3functions.py
@@ -16,9 +16,9 @@ def dh(d, theta, a, alpha):
     
     
 
-def fkine_ur5(q):
+def fkinematics(q):
     """
-    Calcular la cinematica directa del robot UR5 dados sus valores articulares. 
+    Calcular la cinematica directa del robot dados sus valores articulares. 
     q es un vector numpy de la forma [q1, q2, q3, q4, q5, q6]
     """
     # Longitudes (en metros)
@@ -33,42 +33,3 @@ def fkine_ur5(q):
     # Efector final con respecto a la base
     T = 0
     return T
-
-
-def jacobian_ur5(q, delta=0.0001):
-    """
-    Jacobiano analitico para la posicion. Retorna una matriz de 3x6 y toma como
-    entrada el vector de configuracion articular q=[q1, q2, q3, q4, q5, q6]
-    """
-    # Crear una matriz 3x6
-    J = np.zeros((3,6))
-    # Transformacion homogenea inicial (usando q)
-
-    
-    # Iteracion para la derivada de cada columna
-    for i in xrange(6):
-        # Copiar la configuracion articular inicial
-        dq = copy(q)
-        # Incrementar la articulacion i-esima usando un delta
-
-        # Transformacion homogenea luego del incremento (q+delta)
-
-        # Aproximacion del Jacobiano de posicion usando diferencias finitas
-
-    return J
-
-
-def ikine_ur5(xdes, q0):
-    """
-    Calcular la cinematica inversa de UR5 numericamente a partir de la configuracion articular inicial de q0. 
-    """
-    epsilon  = 0.001
-    max_iter = 1000
-    delta    = 0.00001
-
-    q  = copy(q0)
-    for i in range(max_iter):
-        # Main loop
-        pass
-    
-    return q
