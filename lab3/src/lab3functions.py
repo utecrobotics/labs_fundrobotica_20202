b24e50e7848bb082df7d140fd97086bc71f1aab7
Alexander López <lopez.alexander@pucp.edu.pe>
2021-06-05 09:12:37 -0500

    Update lab3functions.py

--- lab3/src/lab3functions.py
+++ lab3/src/lab3functions.py
@@ -16,7 +16,7 @@ def dh(d, theta, a, alpha):
     
     
 
-def fkine_u5(q):
+def fkine_ur5(q):
     """
     Calcular la cinematica directa del robot UR5 dados sus valores articulares. 
     q es un vector numpy de la forma [q1, q2, q3, q4, q5, q6]
